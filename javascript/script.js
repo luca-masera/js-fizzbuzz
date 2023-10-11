53729fd6cdfaeb4353ff15549e74e9d9ba457025
luca-masera <lucamasera1995@gmail.com>
2023-10-11 16:11:43 +0200

    Creazione tabella con html e css

--- javascript/script.js
+++ javascript/script.js
@@ -3,22 +3,23 @@
 let write = 'FizzBuzz';
 let three = 'Fizz';
 let five = 'Buzz';
-let i;
+const quadratoEl = document.getElementById('quadrato');
+
 
 
 for (let i = 1; i < 101; i++ ){
     console.log(i)
+    if((i % 3 === 0) && (i % 5 === 0)){
+        write = 'FizzBuzz'
+        console.log(write)
+    } else if (i % 3 === 0){
+        three = 'Fizz'
+        console.log(three)
+    
+    } else if (i % 5 === 0){
+        five = 'Buzz'
+        console.log(five)
+    }
 }
 
 
-if((i % 3 === 0) && (i % 5 === 0)){
-    write = 'FizzBuzz'
-    console.log(write)
-} else if (i % 3 === 0){
-    three = 'Fizz'
-    console.log(three)
-
-} else if (i % 5 === 0){
-    five = 'Buzz'
-    console.log(five)
-}
